0dbfb6c8e90a3f1ebf9e7d212ffca80eab3acdca
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-04-18 22:01:55 +0200

    optimized order item form

--- ordersapp/forms.py
+++ ordersapp/forms.py
@@ -28,4 +28,4 @@ class OrderItemForm(ModelForm):
         for field_name, field in self.fields.items():
             field.widget.attrs["class"] = "form-control"
 
-        self.fields['product'].queryset = Product.get_items()
+        self.fields['product'].queryset = Product.get_items().select_related()
