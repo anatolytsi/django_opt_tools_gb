a2d4bd9636b13e8ff65bbf11b03c53d6d45856ab
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-04-19 23:39:27 +0200

    added cache and ajax for products

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
-from mainapp.views import ProductsListView
+from mainapp.views import ProductsListView, get_products, products_ajax, category_choose
 
 app_name = 'mainapp'
 
 urlpatterns = [
-    path("", ProductsListView.as_view(), name='index'),
-    path("<int:category_id>/", ProductsListView.as_view(), name="category"),
-    path("page/<int:page>/", ProductsListView.as_view(), name="page"),
-    path("product/<int:pk>/", ProductsListView.as_view(), name="product"),
+    path("", cache_page(3600)(ProductsListView.as_view()), name='index'),
+    # path("category/<int:pk>/ajax/", cache_page(3600)(products_ajax), name='ajax'),
+    path("<int:category_id>/", cache_page(3600)(ProductsListView.as_view()), name="category"),
+    path("page/<int:page>/", cache_page(3600)(ProductsListView.as_view()), name="page"),
+    path("product/<int:pk>/", cache_page(3600)(ProductsListView.as_view()), name="product"),
+    path("category/<int:category_id>/page/<int:page>/", category_choose, name="category_choose"),
 ]
